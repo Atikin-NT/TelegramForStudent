813accfbb321ef2fcfb1636acebc1e2ee59dddbe
Nikita Ivanov <ivanov.n.a.0620@gmail.com>
2022-12-19 22:22:49 +0300

    fix: tmp

--- app.py
+++ app.py
@@ -28,17 +28,17 @@ def main():
         command_list = r.json()["result"]
         for command in command_list:
             if "callback_query" in command:
-                logging.info("Callback query: ", json.dumps(command["callback_query"]["message"]))
+                logging.info(json.dumps(command["callback_query"]["message"]))
                 routes.callback_query(command)
             elif "message" in command:
                 if "entities" in command["message"] and (command["message"]["entities"][0]["type"] == 'bot_command'):
-                    logging.info("Bot command: ", json.dumps(command["message"]))
+                    logging.info(json.dumps(command["message"]))
                     routes.commands(command)
                 else:
-                    logging.info("Input text: " + json.dumps(command["message"]))
+                    logging.info(json.dumps(command["message"]))
                     routes.input_text(command)
             else:
-                logging.critical("Unknown command: ", command)
+                logging.critical("Unknown command: ")
         if len(command_list) != 0:
             last_update_id = command_list[-1]["update_id"] + 1
         print(last_update_id)
